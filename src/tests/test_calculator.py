e3e9b7f45869ffc5a3401f8446f935cca2e7b815
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-05-11 21:06:00 -0400

    ajout test des fonctions division, multiplication et soustraction

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -13,3 +13,23 @@ def test_app():
 
 def test_addition():    
     assert Calculator().addition(2, 3) == 5
+
+def test_subtraction():
+    assert Calculator().subtraction(2, 3) == -1
+    assert Calculator().subtraction(-2, 3) == -5
+    assert Calculator().subtraction(-2, -3) == 1
+    assert Calculator().subtraction(0, -3) == 3
+
+def test_mutiplication():
+    assert Calculator().multiplication(-1, -1) == 1
+    assert Calculator().multiplication(0, 1) == 0
+    assert Calculator().multiplication(1, 0) == 0
+    assert Calculator().multiplication(2, -3) == -6
+    assert Calculator().multiplication(-1, 2) == -2
+    assert Calculator().multiplication(12, 12) == 144
+
+def test_division():
+    # assert throw division par 0
+    assert Calculator().division(6, 3) == 2
+    assert Calculator().division(-6, 6) == -1
+    assert Calculator().division(6, 5) == 1.2
