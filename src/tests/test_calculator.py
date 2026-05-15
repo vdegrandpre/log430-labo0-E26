6db9b9a7624b05d941389dabcdb8b87cf21aedb8
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-05-14 21:05:28 -0400

    documentation des tests

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -12,10 +12,12 @@ def test_app():
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
+# Test de quelques addition simples
 def test_addition():    
     assert Calculator().addition(2, 3) == 5
-    assert Calculator().addition(255, 3234) == 3489
+    assert Calculator().addition(255, -3234) == -2979
 
+# Test de quelques soustraction simples
 def test_subtraction():
     assert Calculator().subtraction(2, 3) == -1
     assert Calculator().subtraction(-2, 3) == -5
@@ -23,6 +25,7 @@ def test_subtraction():
     assert Calculator().subtraction(0, -3) == 3
     assert Calculator().subtraction(3, 3) == 0
 
+# Test de quelques multiplicaitions simples
 def test_mutiplication():
     assert Calculator().multiplication(-1, -1) == 1
     assert Calculator().multiplication(0, 1) == 0
@@ -31,8 +34,9 @@ def test_mutiplication():
     assert Calculator().multiplication(-1, 2) == -2
     assert Calculator().multiplication(12, 12) == 144
 
+# Test de quelques divisions simples
 def test_division():
-    # assert throw division par 0
+    # assert throw division par 0 - omis volontairement
     assert Calculator().division(6, 3) == 2
     assert Calculator().division(-6, 6) == -1
     assert Calculator().division(6, 5) == 1.2
